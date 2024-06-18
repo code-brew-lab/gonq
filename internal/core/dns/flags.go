928db989decbdc1970ae876dd225d8e9e1ad2253
Gökhan Uysal <guysal20@ku.edu.tr>
2024-06-18 21:27:29 +0300

    Header and flags implemented

---
+++ internal/core/dns/flags.go
@@ -0,0 +1,123 @@
+package dns
+
+type (
+	Flags struct { // 16 bits total.
+		isQuery         bool         // [Req + Resp]  Is the message query or response.
+		operationCode   uint8        // [Req]         A four-bit field that specifies the kind of query in this message.
+		isAuthoritative bool         // [Resp]        Is the response from authoritative DNS server.
+		isTruncated     bool         // [Resp]        Is the response truncated or not.
+		isRecursive     bool         // [Req]         Ask DNS server to recursively ask for the domain.
+		canRecursive    bool         // [Resp]        Shows if recursion is available for DNS server.
+		futureUse       uint8        // [Req]         A three-bit future use field.
+		responseCode    ResponseCode // [Resp]        A four-bit response code.
+	}
+
+	FlagsBuilder struct {
+		*Flags
+		errors []error
+	}
+
+	ResponseCode uint8
+)
+
+const (
+	NoError        ResponseCode = iota // #0  OK.
+	FormatError                        // #1  The name server was unable to interpret the query.
+	ServerFailure                      // #2  Server unreachable.
+	NameError                          // #3  Domain name does not exists.
+	NotImplemented                     // #4  Unsupported request query type.
+	Refused                            // #5  Refused for policy reasons of the server.
+)
+
+// NewFlagsBuilder creates a new FlagsBuilder instance with default values
+func NewFlagsBuilder() *FlagsBuilder {
+	return &FlagsBuilder{
+		Flags: newFlags(),
+	}
+}
+
+func newFlags() *Flags {
+	return &Flags{
+		isQuery:     true,
+		isRecursive: true,
+	}
+}
+
+func (fb *FlagsBuilder) SetIsQuery(isQuery bool) *FlagsBuilder {
+	fb.isQuery = isQuery
+	return fb
+}
+
+func (fb *FlagsBuilder) SetOperationCode(operationCode uint8) *FlagsBuilder {
+	fb.operationCode = operationCode
+	return fb
+}
+
+func (fb *FlagsBuilder) SetIsAuthoritative(isAuthoritative bool) *FlagsBuilder {
+	fb.isAuthoritative = isAuthoritative
+	return fb
+}
+
+func (fb *FlagsBuilder) SetIsTruncated(isTruncated bool) *FlagsBuilder {
+	fb.isTruncated = isTruncated
+	return fb
+}
+
+func (fb *FlagsBuilder) SetIsRecursive(isRecursive bool) *FlagsBuilder {
+	fb.isRecursive = isRecursive
+	return fb
+}
+
+func (fb *FlagsBuilder) SetCanRecursive(canRecursive bool) *FlagsBuilder {
+	fb.canRecursive = canRecursive
+	return fb
+}
+
+func (fb *FlagsBuilder) SetFutureUse(futureUse uint8) *FlagsBuilder {
+	fb.futureUse = futureUse
+	return fb
+}
+
+func (fb *FlagsBuilder) SetResponseCode(responseCode ResponseCode) *FlagsBuilder {
+	fb.responseCode = responseCode
+	return fb
+}
+
+func (fb *FlagsBuilder) AddError(err error) {
+	fb.errors = append(fb.errors, err)
+}
+
+func (fb *FlagsBuilder) Build() *Flags {
+	return fb.Flags
+}
+
+func (f *Flags) toUint16() uint16 {
+	var result uint16
+
+	if f.isQuery {
+		result |= 1 << 15
+	}
+
+	result |= uint16(f.operationCode&0xF) << 11
+
+	if f.isAuthoritative {
+		result |= 1 << 10
+	}
+
+	if f.isTruncated {
+		result |= 1 << 9
+	}
+
+	if f.isRecursive {
+		result |= 1 << 8
+	}
+
+	if f.canRecursive {
+		result |= 1 << 7
+	}
+
+	result |= (uint16(f.futureUse) & 0x07) << 4
+	result |= uint16(f.responseCode) & 0x0F
+
+	return result
+}
--- internal/core/dns/header.go
+++ internal/core/dns/header.go
@@ -2,98 +2,87 @@ package dns
 
 import (
 	"encoding/binary"
-	"fmt"
 )
 
 type (
 	Header struct { // 96 bits total
-		ID                    uint16 // [Req + Resp] 16 bit unique request id. Same for the following response.
-		Flags                 Flags
-		QuestionCount         uint16 // [Req] Number of entries inside the question.
-		AnswerCount           uint16 // [Resp] Number of response entries from DNS server.
-		NameServerCount       uint16 // [Req] Number of name server resource records in the authority records section.
-		AdditionalRecordCount uint16 // [Req] Number of resource records in the additional records section.
+		id                    uint16 // [Req + Resp]  Unique request id. Same for the following response.
+		flags                 *Flags // [Req + Resp]  See flags.go
+		questionCount         uint16 // [Req]         Number of entries inside the question.
+		answerCount           uint16 // [Resp]        Number of response entries from DNS server.
+		nameServerCount       uint16 // [Req]         Number of name server resource records in the authority records section.
+		additionalRecordCount uint16 // [Req]         Number of resource records in the additional records section.
 	}
 
-	Flags struct { // 16 bits total.
-		IsQuery         bool  // [Req + Resp] Is the message query or response. Query = False; Response = True;
-		OperationCode   uint8 // [Req] A four-bit field that specifies the kind of query in this message.
-		IsAuthoritative bool  // [Resp] Is the response from authoritative DNS server. Authoritative = True;
-		IsTruncated     bool  // [Resp] Is the response truncated or not.
-		IsRecursive     bool  // [Req] Ask DNS server to recursively ask for the domain.
-		CanRecursive    bool  // [Resp] Shows if recursion is available for DNS server.
-		FutureUse       uint8 // [Req] A three-bit future use field.
-		ResponseCode    uint8 // [Resp] A four-bit response code.
+	HeaderBuilder struct {
+		*Header
+		errors []error
 	}
 )
 
-// NewHeader creates a new Header instance with default values
-func NewHeader(flags Flags, questionCount int) *Header {
-	return &Header{
-		ID:                    0,
-		Flags:                 flags,
-		QuestionCount:         uint16(questionCount),
-		AnswerCount:           0,
-		NameServerCount:       0,
-		AdditionalRecordCount: 0,
+// NewHeaderBuilder creates a new HeaderBuilder instance with default values
+func NewHeaderBuilder() *HeaderBuilder {
+	return &HeaderBuilder{
+		Header: newHeader(),
 	}
 }
 
-// NewFlags creates a new Flags instance with default values
-func NewFlags(isQuery, isRecursive bool) Flags {
-	return Flags{
-		IsQuery:         isQuery,
-		OperationCode:   0,
-		IsAuthoritative: false,
-		IsTruncated:     false,
-		IsRecursive:     isRecursive,
-		CanRecursive:    false,
-		FutureUse:       0,
-		ResponseCode:    0,
+func newHeader() *Header {
+	return &Header{}
+}
+
+func (bh *HeaderBuilder) SetID(id uint16) *HeaderBuilder {
+	bh.id = id
+	return bh
+}
+
+func (bh *HeaderBuilder) SetFlags(flags *Flags) *HeaderBuilder {
+	if flags == nil {
+		return bh
 	}
+
+	bh.flags = flags
+	return bh
+}
+
+func (bh *HeaderBuilder) SetQuestionCount(count uint16) *HeaderBuilder {
+	bh.questionCount = count
+	return bh
+}
+
+func (bh *HeaderBuilder) SetAnswerCount(count uint16) *HeaderBuilder {
+	bh.answerCount = count
+	return bh
+}
+
+func (bh *HeaderBuilder) SetNameServerCount(count uint16) *HeaderBuilder {
+	bh.nameServerCount = count
+	return bh
+}
+
+func (bh *HeaderBuilder) SetAdditionalRecordCount(count uint16) *HeaderBuilder {
+	bh.additionalRecordCount = count
+	return bh
+}
+
+func (bh *HeaderBuilder) AddError(err error) {
+	bh.errors = append(bh.errors, err)
+}
+
+func (bh *HeaderBuilder) Build() *Header {
+	return bh.Header
 }
 
 func (h Header) BinaryMarshaler() ([]byte, error) {
 	var bytes [12]byte
-	flags, err := h.Flags.BinaryMarshaler()
-	if err != nil {
-		return nil, fmt.Errorf("dns: %v", err)
-	}
+	bigEndian := binary.BigEndian
 
-	binary.BigEndian.PutUint16(bytes[0:2], h.ID)
-	copy(bytes[2:4], flags)
-	binary.BigEndian.PutUint16(bytes[4:6], h.QuestionCount)
-	binary.BigEndian.PutUint16(bytes[6:8], h.AnswerCount)
-	binary.BigEndian.PutUint16(bytes[8:10], h.NameServerCount)
-	binary.BigEndian.PutUint16(bytes[10:12], h.AdditionalRecordCount)
+	bigEndian.PutUint16(bytes[0:2], h.id)
+	bigEndian.PutUint16(bytes[2:4], h.flags.toUint16())
+	bigEndian.PutUint16(bytes[4:6], h.questionCount)
+	bigEndian.PutUint16(bytes[6:8], h.answerCount)
+	bigEndian.PutUint16(bytes[8:10], h.nameServerCount)
+	bigEndian.PutUint16(bytes[10:12], h.additionalRecordCount)
 
 	return bytes[:], nil
-}
-
-func (f Flags) BinaryMarshaler() ([]byte, error) {
-	var flags uint16
-
-	if f.IsQuery {
-		flags |= 1 << 15
-	}
-	flags |= uint16(f.OperationCode&0xF) << 11
-	if f.IsAuthoritative {
-		flags |= 1 << 10
-	}
-	if f.IsTruncated {
-		flags |= 1 << 9
-	}
-	if f.IsRecursive {
-		flags |= 1 << 8
-	}
-	if f.CanRecursive {
-		flags |= 1 << 7
-	}
-	flags |= uint16(f.FutureUse&0x7) << 4
-	flags |= uint16(f.ResponseCode & 0xF)
-
-	bytes := make([]byte, 2)
-	binary.BigEndian.PutUint16(bytes, flags)
-
-	return bytes, nil
 }
